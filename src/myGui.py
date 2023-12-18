9c3a4095ab3bbb2a9bee10a8afc58bd1375012a2
MargaridaEstrela <margaridaestrela@tecnico.ulisboa.pt>
2023-12-18 09:03:42 +0000

    geat(gui): gui tested, need some improves

--- src/myGui.py
+++ src/myGui.py
@@ -4,9 +4,9 @@ import random
 import numpy as np
 import cv2
 
-elmoIp = "192.168.1.92"
+elmoIp = "192.168.0.101"
 elmoPort = 4000
-clientIp = "192.168.1.84"
+clientIp = "192.168.0.114"
 
 emotions = ["angry", "disgust", "fear", "happy", "sad", "surprise", "neutral"]
 play = 1
@@ -16,6 +16,9 @@ bad_face = ["cry", "confused", "anger"]
 good_face = ["normal", "rolling_eyes", "thinking"]
 awesome_face = ["love", "images/simon_images/stars.gif", "blush"]
 
+# default params
+default_pan = 0
+default_tilt = 0
 
 # when the respective buttons are clicked, one of the sounds in the list will be played at random
 goodSoundList = []  # the list of sounds containing the necessary sounds for the good buttons
@@ -23,8 +26,7 @@ badSoundList = []   # the list of sounds containing the necessary sounds for the
 awesomeSoundList = []   # the list of sounds containing the necessary sounds for the awesome buttons
 
 
-
-myElmo = ElmoServer(elmoIp, elmoPort, clientIp, True)
+myElmo = ElmoServer(elmoIp, elmoPort, clientIp, False)
 
 sg.theme('DarkBlue')   # Add a touch of color
 
@@ -33,8 +35,8 @@ sg.theme('DarkBlue')   # Add a touch of color
 #lst = sg.Combo(imageList, font=('Arial Bold', 14),  expand_x=True, enable_events=True,  readonly=False, key='-COMBO-')
 layout = [      
     [sg.Button("Look Player1", size=(15, 1)), sg.Button("Look Player2", size=(15, 1))],
-    [sg.Text("Pan", size=(3, 1)), sg.InputText(key="pan_value", size=(18, 1)), sg.Button("Set", key="SetPan", size=(8, 1))],
-    [sg.Text("Tilt", size=(3, 1)), sg.InputText(key="tilt_value", size=(18, 1)), sg.Button("Set", key="SetTilt", size=(8, 1))],
+    [sg.Text("Pan", size=(3, 1)), sg.InputText(key="pan_value", size=(18, 1)), sg.Button("Set", key="SetPan", size=(8, 1)), sg.Text('', size=(5, 1)), sg.Button("Set Default", key="SetDefaultPan", size=(10, 1))],
+    [sg.Text("Tilt", size=(3, 1)), sg.InputText(key="tilt_value", size=(18, 1)), sg.Button("Set", key="SetTilt", size=(8, 1)),  sg.Text('', size=(5, 1)), sg.Button("Set Default", key="SetDefaultTilt", size=(10, 1))],
     [sg.Button("Toggle Behaviour", size=(15, 1)), sg.Button("Toggle Motors", size=(15, 1))],
     [sg.Text('', size=(1, 1))],
     [sg.Text("Game")],
@@ -83,28 +85,31 @@ while True:
         else:
             window['Toggle Motors'].update(button_color=('white', 'red'))
     
-    if event == "LookPlayer1":
+    if event == "Look Player1":
         print("Looking at player 1")
-        myElmo.moveLeft()
+        myElmo.moveLeft(default_pan, default_tilt)
     
-    elif event == "LookPlayer2":
-        myElmo.moveRight()
+    elif event == "Look Player2":
+        myElmo.moveRight(default_pan, default_tilt)
         print("Looking at player 2")
-    
-    if event == "SetImage":
-        print("Setting Image")
-        myElmo.setImage(values['-COMBO-'])    # this is to set the image based on the dopdown list
-        myElmo.setImage(random.choice(imageList))       # this is to set the image randomly
    
     if event == "SetPan":
         value = values["pan_value"]
         if value: 
             myElmo.movePan(value)
+            default_pan = value
+    
+    if event == "SetDefaultPan":
+        myElmo.movePan(default_pan)
 
     if event == "SetTilt":
         value = values["tilt_value"]
         if value:
             myElmo.moveTilt(value)
+            default_tilt = value
+
+    if event == "SetDefaultTilt":
+        myElmo.movePan(default_tilt)
 
     if event == "Intro":
         myElmo.introduceGame()
@@ -125,13 +130,13 @@ while True:
         window['player'+ str(play % 2 + 1)+"_points"].update(points[str(play % 2)])
 
     elif event == "Next":
+        play += 1
         if play > len(emotions):
             myElmo.movePan(0)
             myElmo.endGame()
         else:
             emotion = emotions[play-1]
             myElmo.sayEmotion(emotion)
-            play += 1
 
     elif event == "Winner":
         winner = max(points, key=points.get)
--- src/server.py
+++ src/server.py
@@ -5,284 +5,210 @@ import socket
 import requests
 import cv2
 import numpy as np
-
-face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-
-default_pan = 30
-default_tilt = 0
-
-#list with all the emotions we will ask
-emotions = ["angry", "disgust", "fear", "happy", "sad", "surprise", "neutral"]
-
-#dictionary with all the emotions possible to the respective accuracy
-accuracy_dict = {50: "thinking", 80: "normal", 95: "love"}
-
-points = {"0": 0, "1": 0}
+import time 
 
 
-def detect_faces(frame):
-    """
-    Detect faces in a given frame using OpenCV.
-    """
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)  
-    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+class ElmoServer:
 
-    #filter out faces smaller than x pixels by y pixels
-    faces = list(filter(lambda face: face[2] > 300 and face[3] > 300, faces))
+    def __init__(self, elmoIp="192.168.0.101", elmoPort=4000, clientIp="192.168.0.114", debug=False):
+        self.elmoIp = elmoIp
+        self.elmoPort = elmoPort
+        self.clientIp = clientIp
+
+        # set the motors and behaviour to false
+        self.activeBehaviour = False
+        self.activeMotors = False
+        self.sendRequestCommand("enable_behaviour", name="look_around", control=False)
+        self.sendRequestCommand("set_tilt_torque", control=False)
+        self.sendRequestCommand("set_pan_torque", control=False)
+
+        if debug == False:
+            self.connectElmo()
+            self.debug = False
+        else:
+            print("debug mode has been activated")
+            self.debug = True
     
-    return faces
+    def connectElmo(self):
+        # this will start the socket used to communicate with elmo
+        self.elmoSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.elmoSocket.bind((self.clientIp,self.elmoPort))
+        print("Connecting as okay")
+        
 
 
-def grabImage(url="http://192.168.0.101:8080/stream.mjpg"):
-    """
-    Grab the image from the camera
-    """
-    response = requests.get(url, stream=True)
+    def sendMessage(self, message):
+        """
+        This will send a message to elmo
+        """
 
-    if response.status_code == 200:
-        # maens that it is a valid response
-        stream = response.iter_content(chunk_size=1024)
-        bytes_ = b''
-        for chunk in stream:
-            bytes_ += chunk
-            a = bytes_.find(b'\xff\xd8')
-            b = bytes_.find(b'\xff\xd9')
-            if a != -1 and b != -1:
-                jpg = bytes_[a:b+2]
-                bytes_ = bytes_[b+2:]
-                frame = cv2.imdecode(np.fromstring(jpg, dtype=np.uint8), cv2.IMREAD_COLOR)
-                break
-
-        return frame
-    return None
-
-
-def connectElmo(elmo_ip="192.168.0.101", port=4000, client_ip="192.168.0.114"):
-    """
-    This will start the socket used to communicate with elmo
-    """
-    elmoSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    elmoSocket.bind((client_ip,port))
-
-    return elmoSocket
-
-
-def getAction():
-    """
-    This will request a command to the user
-    """
-    command = input("Comand: ")
-    value = input("Value: ")
-
-    action =f"{command}::{value}"
-    print("[REQUEST]: ", action)
-
-    return action
-
-
-def getAction(command, value):
-    """
-    This will return a request
-    """
-    action =f"{command}::{value}"
-    print("[REQUEST]: ", action)
-
-    return action
-
-
-def sendMessage(message):
-    """
-    This will send a message to elmo
-    """
-    print("Sending message: " + message)
-    elmoSocket.sendto(message.encode('utf-8'), server)
-    data, addr = elmoSocket.recvfrom(1024)     # wait for the response from elmo
-    data = data.decode('utf-8')
-    print("  Received from server: " + data)
-    return data
-
-
-def showImage(image, faces):
-    # this will show the image with the faces
-    for (x, y, w, h) in faces:
-        cv2.rectangle(image, (x,y), (x+w, y+h), (255, 0, 0), 2)
+        if self.debug == True:
+            # means that I am in debug mode and do not want to send the message
+            print("Fake Sending: " + message)
+            return "fake"
+            
+        print("Sending message: " + message)
+        self.elmoSocket.sendto(message.encode('utf-8'), (self.elmoIp, self.elmoPort))
+        data, addr = self.elmoSocket.recvfrom(1024)     # wait for the response from elmo
+        data = data.decode('utf-8')
+        print("  Received from server: " + data)
+        return data
     
-    # downscale the image by 0.5
-    show_image = cv2.resize(image, (0,0), fx=0.5, fy=0.5)
-    cv2.imshow('image', show_image)
-    key = cv2.waitKey(1)
-    if key == ord('q'):
-        exit()
+
+    def getImageList(self):
+        # this will grab the list of images from elmo
+        data = self.sendMessage("getImage::none")
+        self.imageList = eval(data)
+        return self.imageList
 
 
-def loop():
+    def setImage(self, image_name):
+        # this will set the image on elmo
+        data = self.sendMessage(f"image::{image_name}")
+
+
+    def movePan(self, angle):
+        # this will move elmo's pan
+        data = self.sendMessage(f"pan::{angle}")
+
+
+    def moveTilt(self, angle):
+        # this will move elmo's tilt
+        data = self.sendMessage(f"tilt::{angle}")
+
+
+    def moveLeft(self, default_pan, default_tilt):
+        # this will move elmo left
+        default_pan = -default_pan
+        data = self.sendMessage(f"pan::{default_pan}")
+        data = self.sendMessage(f"tilt::{default_tilt}")
+
+
+    def moveRight(self, default_pan, default_tilt):
+        # this will move elmo left
+        data = self.sendMessage(f"pan::{default_pan}")
+        data = self.sendMessage(f"tilt::{default_tilt}")
+        
+
+    def grabImage(self):
+        # grab the image from the camera
+        if self.debug == False:
+            url = f"http://{self.elmoIp}:8080/stream.mjpg"
+            response = requests.get(url, stream=True)
+
+            if response.status_code == 200:
+                # maens that it is a valid response
+                stream = response.iter_content(chunk_size=1024)
+                bytes_ = b''
+                for chunk in stream:
+                    bytes_ += chunk
+                    a = bytes_.find(b'\xff\xd8')
+                    b = bytes_.find(b'\xff\xd9')
+                    if a != -1 and b != -1:
+                        jpg = bytes_[a:b+2]
+                        bytes_ = bytes_[b+2:]
+                        frame = cv2.imdecode(np.fromstring(jpg, dtype=np.uint8), cv2.IMREAD_COLOR)
+                        break
+                
+                # resize the image to 480, 640
+                frame = cv2.resize(frame, (640, 480))
+                return frame
+        return np.full((480, 640, 3), 26, dtype=np.uint8)
+
+
+    def introduceGame(self):
+        data = self.sendMessage(f"sound::intro.wav")
+        time.sleep(6)
+
+        data = self.sendMessage(f"sound::rules.wav")
+        time.sleep(6)
+
+        data = self.sendMessage(f"sound::ready.wav")
+        time.sleep(6)
+
+
+    def playGame(self):
+        # this will start the game
+        data = self.sendMessage(f"game::on")
     
-    image = grabImage()
-    faces = detect_faces(image)
 
-    # show the image with rectanges on the faces
-    showImage(image, faces)
+    def sayEmotion(self, emotion):
+        data = self.sendMessage(f"sound::{emotion}.wav")
 
 
-def userLoop():
-    data = sendMessage("getImage::off")
-    imageList = eval(data)
+    def endGame(self):
+        data = self.sendMessage(f"sound::end_game.wav")
 
-    counter = 0
-    while True:
-        print("sending this image: ", imageList[counter])
-        data = sendMessage(f"image::{imageList[counter]}")
-        counter +=1
-        if counter > len(imageList)-1:
-            counter = 0
+
+    def congratsWinner(self):
+        data = self.sendMessage(f"sound::winner.wav")
+
+
+    def closeAll(self):
+        # this will end the game
+        data = self.sendMessage(f"game::off")
+
+        time.sleep(1)
+        if self.debug == False:
+            self.elmoSocket.shutdown(socket.SHUT_RDWR)
+            self.elmoSocket.close()
+            return 
+        return
+        # close all
+    
+
+    def playSound(self, sound):
+        data = self.sendMessage(f"sound::{sound}")
+
+    def takePicture(self):
+        # show 3, 2, 1 and take a picture
+        data = self.sendMessage(f"icon::call.png")
         time.sleep(1)
 
+        data = self.sendMessage(f"icon::music.png")
+        time.sleep(1)
 
-def setDefaultAngles():   
-    # set default pan
-    pan_message = getAction("pan", default_pan)
-    sendMessage(pan_message)
+        data = self.sendMessage(f"icon::call.png")
+        time.sleep(1)
 
-    params["pan"] = default_pan
-   
-    # set default tilt
-    tilt_message = getAction("tilt", default_tilt)
-    sendMessage(tilt_message)
+        data = self.sendMessage(f"icon::elmo_idm.png")
+        time.sleep(1)
 
-    params["tilt"] = default_tilt
+        return self.grabImage()
 
 
-def adjusParameters():
+    def analysePicture(self, frame, emotion):
+        face_analysis = DeepFace.analyze(frame)
+        print(face_analysis[0]["emotion"][emotion])
+        return face_analysis[0]["emotion"][emotion]
 
-    while True:
+
+    def toggleMotors(self):
+        # this will enable elmo's motors
+        data = self.sendMessage(f"motors::enable")
+        self.activeMotors = not self.activeMotors
+        print("Motors are now: ", self.activeMotors)
+        self.sendRequestCommand("set_tilt_torque", control=self.activeMotors)
+        self.sendRequestCommand("set_pan_torque", control=self.activeMotors)
+
+
+    def toggleBehaviour(self):
+        # self.sendRequestCommand("disable_behaviour", name=name)
+        self.activeBehaviour = not self.activeBehaviour
+        self.sendRequestCommand("enable_behaviour", name="look_around", control=self.activeBehaviour)
+        print("toggleBehaviour")
+    
+
+    def sendRequestCommand(self, command, **kwargs):
         try:
-            command = input("Command: ")
-
-        except KeyboardInterrupt:
-            print("\nCtrl+C detected. Exiting ...")
-            elmoSocket.close()
-            exit()
-            
-        if command == 'game':
-            print("All params set!")
-            break
-
-        if command == "pan" or command == "tilt":
-            value = input("Value: ")
-            param_message = getAction(command, value)
-            sendMessage(param_message)
-            params[command] = int(value)
-
-
-def introduceGame():
-    intro_message = getAction("sound", "intro.wav")
-    sendMessage(intro_message)
-    time.sleep(6)
-    rules_message = getAction("sound", "rules.wav")
-    sendMessage(rules_message)
-    time.sleep(6)
-    ready_message = getAction("sound", "ready.wav")
-    sendMessage(ready_message)
-    time.sleep(6)
-
-
-def takePictureSequence():
-    # show 3, 2, 1, picture
-    icon_3 = getAction("icon", "call.png")
-    sendMessage(icon_3)
-    time.sleep(1)
-    icon_2 = getAction("icon", "music.png")
-    sendMessage(icon_2)
-    time.sleep(1)
-    icon_1 = getAction("icon", "call.png")
-    sendMessage(icon_1)
-    time.sleep(1)
-    camera = getAction("icon", "elmo_idm.png")
-    sendMessage(camera)
-
-
-def play_game():
-
-
-    #adjusParameters()
-
-    print("Starting game...")
-
-    # introduce game
-    introduceGame()
-
-    time.sleep(2)
-
-    setDefaultAngles()
-
-    play = 1
-
-    # start game
-    for emotion in emotions:
-        emotion_request = getAction("sound", f"{emotion}.wav")
-        sendMessage(emotion_request)
-
-        time.sleep(3)
-
-        # show 3, 2, 1, take a picture
-        #takePictureSequence()
-        #frame = grabImage()
-        #face_analysis = DeepFace.analyze(frame)
-        
-        # show feedback
-        #accuracy = face_analysis["emotion"][emotion]
-        accuracy = 70
-        if (accuracy < 80):
-            image = accuracy_dict[50]
-        elif (accuracy < 95):
-            image = accuracy_dict[80]
-        else:
-            image = accuracy_dict[95]
-
-        image_display = getAction("image", image)
-        sendMessage(image_display)
-
-        # points
-        points[str(play%2)] += accuracy
-
-        # play sound randomly
-        feedback_sound = getAction("sound", "correct.wav")
-        sendMessage(feedback_sound)
-
-        time.sleep(4)
-
-        image_display = getAction("image", "normal")
-        sendMessage(image_display)
-
-        # change player
-        params["pan"] = -params["pan"]
-        pan_message = getAction("pan", params["pan"])
-        sendMessage(pan_message)
-        play += 1
-
-        time.sleep(2)
-
-    # end game sound
-    print("Ending game...")
-    final_sound = getAction("sound", "end_game.wav")
-    sendMessage(final_sound)
-
-    time.sleep(5)
-
-    # congrats the winer
-    winner = max(points, key=points.get)
-    params["pan"] = abs(params["pan"])
-
-    if winner == "0":
-        params["pan"] = - params["pan"]
-
-    pan_message = getAction("pan", params["pan"])
-    sendMessage(pan_message)
-
-    winner_sound = getAction("sound", "winner.wav")
-    sendMessage(winner_sound)
-
-    time.sleep(3) 
+            url = "http://" + self.elmoIp + ":8001/command"
+            kwargs["op"] = command
+            print(kwargs)
+            res = requests.post(url, json=kwargs, timeout=1).json()
+            if not res["success"]:
+                self.on_error(res["message"])
+        except Exception as e:
+            print(e)
 
 
 if __name__=='__main__':
@@ -290,26 +216,5 @@ if __name__=='__main__':
     elmoIp = "192.168.0.101"
     elmoPort = 4000
     clientIp = "192.168.0.114"
-    server = (elmoIp, elmoPort)
 
-    elmoSocket = connectElmo(elmoIp, elmoPort, clientIp)
-
-    params = {"pan": 0, "tilt": 0}
-
-    pan_message = getAction("pan", params["pan"])
-    sendMessage(pan_message)
-
-    tilt_message = getAction("tilt", params["tilt"])
-    sendMessage(tilt_message) 
-
-    image_display = getAction("image", "normal")
-    sendMessage(image_display)
-
-    play_game()
-
-    # send message to indicate the game is over
-    end_game_message = getAction("game", "off")
-    sendMessage(end_game_message)
-
-    # closing socket
-    elmoSocket.close()
+    myElmo = ElmoServer(elmoIp, elmoPort, clientIp)
